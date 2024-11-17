393e51020cdb6340c94428cf068480db28e1ad3b
devjuninho19 <fareisorrir@gmail.com>
2024-11-17 19:35:23 -0300

    página admin finalizada!

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import "./admin.css"
+import React, { useState } from "react";
+import { auth } from "../../Firebaseconnection";
+import { signOut } from "firebase/auth";
+import "./admin.css";
 const index = () => {
-  return (
-    <div>
-        <h1>Pagina admin</h1>
-    </div>
-  )
-}
+  const [tarefaInput, setTarefaInput] = useState("");
 
-export default index
+  function handleRegister(e) {
+    e.preventDefault();
+    alert("clicou!");
+  }
+
+async function logout(){
+await signOut(auth)
+  }
+  return (
+    <div className="admin-container">
+      <h1>Minhas Tarefas</h1>
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite sua tarefa"
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+        />
+        <button className="btn-register" type="submit">Registrar</button>
+      </form>
+
+      <article className="list">
+        <p>Estudar React</p>
+        <div>
+          <button>Editar</button>
+          <button className="btn-concluir">Finalizar</button>
+        </div>
+      </article>
+      <button className="btn-sair" onClick={logout}>Sair</button>
+    </div>
+  );
+};
+
+export default index;
